a0c3e700022fc71419e8f83831775452001cf437
uWillan <mauriciowas.ms@gmail.com>
2022-06-21 17:13:37 -0300

    Atividade1

--- Atividade.js
+++ Atividade.js
@@ -1,18 +1,18 @@
-var listaDePecas = ["Amortecedor","Motor","Correia dentada","Filtro de ar","Peneu 14","Farol"]; //Array
+var listaDePecas = ["Amortecedor","Motor","Correia dentada","Filtro de ar","Peneu 14","Farol"] //Array
 if (listaDePecas.length > 6){
-    console.log("Não é possivel cadastrar por execesso de peças");
+    console.log("Não é possivel cadastrar por execesso de peças")
 }else{
     console.log("Limite ok")
 }
 
-let peso = 60;
+let peso = 60
 if(peso >= 100){
     console.log("Peso permetido")
 }else{
     console.log("O peso está abaixo do normal")
 }
 
-let peca = "Farol";
+let peca = "Farol"
 if (peca.length < 3){
     console.log("O nome da peça é muito pequeno")
 }else{
